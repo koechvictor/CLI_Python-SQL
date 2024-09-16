0595b03264e7358ffcacc86532530e5eb245e208
koech victor <kipkiruiv408@gmail.com>
2024-09-16 19:28:25 +0000

    db initialization

--- library_management/models.py
+++ library_management/models.py
@@ -1,5 +1,8 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+
+Base = declarative_base()
 
 
 class Book:
