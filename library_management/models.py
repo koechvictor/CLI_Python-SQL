fa324a1c26bf469d4b8603a9ad0aba5005b5db0d
koech victor <kipkiruiv408@gmail.com>
2024-09-16 19:13:33 +0000

    tables creation

---
+++ library_management/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Book:
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    author = Column(String, index=True)
+
+class Member:
+    __tablename__ = 'members'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+
+class Borrow:
+    __tablename__ = 'borrows'
+
+    id = Column(Integer, primary_key=True, index=True)
+    member_id = Column(Integer, ForeignKey('members.id'))
+    book_id = Column(Integer, ForeignKey('books.id'))
+
+    member = relationship("Member")
+    book = relationship("Book")
