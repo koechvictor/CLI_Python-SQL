9f060a2c2654b077e26b91821a0883e4c853944c
koech victor <kipkiruiv408@gmail.com>
2024-09-16 20:12:12 +0000

    update feature

--- library_management/cli.py
+++ library_management/cli.py
@@ -18,7 +18,26 @@ def init_db():
     Base.metadata.create_all(bind=engine)
     click.echo("Database initialized.")
 
+@click.command()
+@click.option('--book_id', prompt='Book ID', help='The ID of the book to update.')
+@click.option('--title', prompt='New title', help='The new title of the book.')
+@click.option('--author', prompt='New author', help='The new author of the book.')
+def update_book(book_id, title, author):
+    """Update a book."""
+    db = SessionLocal()
+    book = db.query(Book).filter(Book.id == book_id).first()
+    if book:
+        book.title = title
+        book.author = author
+        db.commit()
+        db.refresh(book)
+        click.echo(f"Book ID {book_id} updated to '{title}' by {author}.")
+    else:
+        click.echo(f"Book ID {book_id} not found.")
+    db.close()
+
 cli.add_command(init_db)
+cli.add_command(update_book)
 
 if __name__ == '__main__':
     cli()
