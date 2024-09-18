a0c736970d2cefceaa049567892d0a12cdde34a0
koech victor <kipkiruiv408@gmail.com>
2024-09-18 12:51:21 +0000

    database update

--- library_management/library/models.py
+++ library_management/library/models.py
@@ -4,18 +4,25 @@ from datetime import datetime
 
 Base = declarative_base()
 
-class Author(Base):
-    __tablename__ = 'authors'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    books = relationship('Book', back_populates='author')
+#class Author(Base):
+ #   __tablename__ = 'authors'
+  #  id = Column(Integer, primary_key=True)
+   # name = Column(String, nullable=False)
+    #books = relationship('Book', back_populates='author')
+
+#class Book(Base):
+ #   __tablename__ = 'books'
+  #  id = Column(Integer, primary_key=True)
+   # title = Column(String, nullable=False)
+    #author_id = Column(Integer, ForeignKey('authors.id'))
+    #author = relationship('Author', back_populates='books')
+    #is_borrowed = Column(Boolean, default=False)
 
 class Book(Base):
     __tablename__ = 'books'
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
-    author_id = Column(Integer, ForeignKey('authors.id'))
-    author = relationship('Author', back_populates='books')
+    author_name = Column(String, nullable=False)
     is_borrowed = Column(Boolean, default=False)
 
 class User(Base):
