ff049e69f2f25a8b86ec722665bb69aa19af8a32
koech victor <kipkiruiv408@gmail.com>
2024-09-18 06:12:24 +0000

    database update

--- library_management/models.py
+++ library_management/models.py
@@ -1,30 +1,35 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
+from sqlalchemy.orm import relationship, declarative_base
+from datetime import datetime
 
 Base = declarative_base()
 
+class Author(Base):
+    __tablename__ = 'authors'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    books = relationship('Book', back_populates='author')
 
-class Book:
+class Book(Base):
     __tablename__ = 'books'
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    author_id = Column(Integer, ForeignKey('authors.id'))
+    author = relationship('Author', back_populates='books')
+    is_borrowed = Column(Boolean, default=False)
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    author = Column(String, index=True)
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    borrowed_books = relationship('Borrow', back_populates='user')
 
-class Member:
-    __tablename__ = 'members'
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
-
-class Borrow:
+class Borrow(Base):
     __tablename__ = 'borrows'
-
-    id = Column(Integer, primary_key=True, index=True)
-    member_id = Column(Integer, ForeignKey('members.id'))
+    id = Column(Integer, primary_key=True)
     book_id = Column(Integer, ForeignKey('books.id'))
-
-    member = relationship("Member")
-    book = relationship("Book")
+    user_id = Column(Integer, ForeignKey('users.id'))
+    borrow_date = Column(DateTime, default=datetime.utcnow)
+    return_date = Column(DateTime, nullable=True)
+    book = relationship('Book')
+    user = relationship('User', back_populates='borrowed_books')
