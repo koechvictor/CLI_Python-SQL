10d384b5eb9864195c1ffa5633f5e7abcacb1c07
koech victor <kipkiruiv408@gmail.com>
2024-09-18 12:52:05 +0000

    cli functions update

--- library_management/library/cli.py
+++ library_management/library/cli.py
@@ -1,6 +1,6 @@
 import click
 from library.database import SessionLocal, init_db
-from library.models import Author, Book, User, Borrow
+from library.models import Book, User, Borrow
 
 @click.group()
 def cli():
@@ -14,44 +14,70 @@ def init():
 
 @click.command()
 @click.option('--title', prompt='Book title', help='The title of the book.')
-@click.option('--author_id', prompt='Author ID', help='The ID of the author.')
-def add_book(title, author_id):
+@click.option('--author_name', prompt="Author's Name", help='The ID of the author.')
+def add_book(title, author_name):
     """Add a new book."""
     session = SessionLocal()
-    book = Book(title=title, author_id=author_id)
+    book = Book(title=title, author_name=author_name)
     session.add(book)
     session.commit()
     session.close()
-    click.echo(f'Added book {title}.')
+    click.echo(f'Added book {title} written by {author_name}.')
+
+#@click.command()
+#@click.option('--title', prompt='Book title', help='The title of the book.')
+#@click.option('--author_id', prompt='Author ID', help='The ID of the author.')
+#def add_book(title, author_id):
+ #   """Add a new book."""
+  #  session = SessionLocal()
+   # book = Book(title=title, author_id=author_id)
+    #session.add(book)
+    #session.commit()
+    #session.close()
+    #click.echo(f'Added book {title}.')
+
+#@click.command()
+#@click.option('--name', prompt='Author name', help='The name of the author.')
+#def add_author(name):
+ #   """Add a new author."""
+  #  session = SessionLocal()
+   # author = Author(name=name)
+    #session.add(author)
+    #session.commit()
+    #session.close()
+    #click.echo(f'Added author {name}.')
+
+#@click.command()
+#@click.option('--book_id', prompt='Book ID', help='The ID of the book.')
+#@click.option('--title', prompt='New Book title', help='The new title of the book.')
+#def update_book(book_id, title):
+ #   """Update a book."""
+ #   session = SessionLocal()
+  #  book = session.query(Book).filter(Book.id == book_id).first()
+   # if book:
+    #    book.title = title
+     #   session.commit()
+      #  click.echo(f'Updated book ID {book_id} to title {title}.')
+    #else:
+     #   click.echo(f'Book ID {book_id} not found.')
+    #session.close()
 
 @click.command()
-@click.option('--name', prompt='Author name', help='The name of the author.')
-def add_author(name):
-    """Add a new author."""
-    session = SessionLocal()
-    author = Author(name=name)
-    session.add(author)
-    session.commit()
-    session.close()
-    click.echo(f'Added author {name}.')
-
-@click.command()
-@click.option('--book_id', prompt='Book ID', help='The ID of the book to update.')
-@click.option('--title', prompt='New title', help='The new title of the book.')
-@click.option('--author', prompt='New author', help='The new author of the book.')
-def update_book(book_id, title, author):
+@click.option('--book_id', prompt='Book ID', help='The ID of the book.')
+@click.option('--title', prompt='New Book title', help='The new title of the book.')
+@click.option('--author_name', prompt="New Author's Name", help='The Name of the author.')
+def update_book(book_id, title, author_name):
     """Update a book."""
-    db = SessionLocal()
-    book = db.query(Book).filter(Book.id == book_id).first()
+    session = SessionLocal()
+    book = session.query(Book).filter(Book.id == book_id).first()
     if book:
         book.title = title
-        book.author = author
-        db.commit()
-        db.refresh(book)
-        click.echo(f"Book ID {book_id} updated to '{title}' by {author}.")
+        book.author_name = author_name
+        session.commit()
+        click.echo(f'Updated book ID {book_id} to title {title}. and author {author_name}.')
     else:
-        click.echo(f"Book ID {book_id} not found.")
-    db.close()
+        click.echo(f'Book ID {book_id} not found.')
+    session.close()
 
 @click.command()
 def report_available_books():
@@ -61,14 +87,14 @@ def report_available_books():
     if available_books:
         click.echo("Available Books:")
         for book in available_books:
-            click.echo(f'ID: {book.id}, Title: {book.title},Author: {book.author.name}')
+            click.echo(f'ID: {book.id}, Title: {book.title},Author: {book.author_name}')
     else:
         click.echo("No books are currently available.")
     session.close()
 
 cli.add_command(init)
 cli.add_command(add_book)
-cli.add_command(add_author)
+#cli.add_command(add_author)
 cli.add_command(update_book)
 cli.add_command(report_available_books)
 
